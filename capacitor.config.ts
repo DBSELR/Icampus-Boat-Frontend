6418e2a4e4fdfcd721eaac134f17e7bcd3dbe289
DBASE <89074505+DBSELR@users.noreply.github.com>
2026-07-01 16:27:11 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Icampus-Boat-Frontend',
+  webDir: 'dist'
+};
+
+export default config;
