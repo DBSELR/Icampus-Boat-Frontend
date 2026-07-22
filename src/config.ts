e3070ec61f6658d7533f47afcd2b95c6fb8f9b30
DBASE <89074505+DBSELR@users.noreply.github.com>
2026-07-22 18:33:41 +0530

    hh

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_BASE = import.meta.env.DEV
-    ? "https://localhost:7034/api/"
+    ? "https://localhost:44351/api/"
     : import.meta.env.VITE_API_BASE ?? "https://test.dbasesolutions.in/api/";
